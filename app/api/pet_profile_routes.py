198b95f69df7b1f5797005a8704513c3a430e184
Tony <iamt.he@yahoo.com>
2023-06-16 18:30:10 -0700

    finished posting weight goal to pet profile route

--- app/api/pet_profile_routes.py
+++ app/api/pet_profile_routes.py
@@ -38,7 +38,7 @@ def get_current_profiles():
 @login_required
 def get_one_profile(id):
     """
-    Gets one pet profile of current user
+    Gets one pet profile of any user
     """
     single_profile=Profile.query.get(id)
     if not single_profile:
@@ -74,13 +74,14 @@ def edit_pet(id):
     Update a pet profile for the current user 
     """ 
     pet_profile=Profile.query.get(id)
-
+# check if pet exists
     if not pet_profile:
         return jsonify({'error': 'Meal log not found'}), 404
     
     form=PetForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
+    # check if profile belongs to current user
     if pet_profile.user_id==current_user.id:
         pet_profile.dog_name=form.dog_name.data
         pet_profile.breed=form.breed.data
@@ -102,20 +103,23 @@ def delete_pet(id):
 
     if not pet_profile:
         return jsonify({'error': 'Pet profile not found'}), 404
+    
     if pet_profile.user_id==current_user.id:
         db.session.delete(pet_profile)
         db.session.commit()
         return jsonify({'message': 'Pet profile log deleted successfully!'}), 200
     return jsonify({'error': 'You do not own this pet profile.'}), 401
+
 #PET MEAL ROUTES TO MAKE A NEW MEAL LOG
 @profile_routes.route('/<int:id>/meals/new', methods=["POST"])
 @login_required
 def post_one_meal(id):
     """
     Post one meal log for one pet by id
-    make s
     """
+    # get one pet profile by id
     pet_profile=Profile.query.get(id)
+    # get all pet profiles that the user owns
     pet_profiles=Profile.query.filter(Profile.user_id==current_user.id).all()
 
     form = MealForm()
@@ -123,6 +127,7 @@ def post_one_meal(id):
     # print("printing the profffille object=====-=-=------------",pet_profile.to_dict())
     if not pet_profile:
         return jsonify({'error': 'Pet profile not found'}), 404
+    
     if any(current_user.id == pet_profile.user_id for profile in pet_profiles):
         new_pet_meal=Meal_Log(
             profile_id=pet_profile.id,
@@ -143,22 +148,28 @@ def post_one_meal(id):
 @login_required
 def post_one_calorie_goal(id):
     """
-    Post one caloric goal for current user
+    Post one caloric goal for pet by id
     """
 
     form=CalorieForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
-    profile_id=Profile.query.get(id)
-    print("calories route print pet id======================================", profile_id.id)
-    new_calorie_goal=Calorie_Goal(
-        profile_id=profile_id.id,
-        calorie_goal=form.calorie_goal.data,
-        date=form.date.data
-    )
-    db.session.add(new_calorie_goal)
-    db.session.commit()
-    return new_calorie_goal.to_dict()
+    pet_profile=Profile.query.get(id)
+    # print("calories route print pet id======================================", profile_id.id)
+    pet_profiles=Profile.query.filter(Profile.user_id==current_user.id).all()
+
+    if not pet_profile:
+        return jsonify({'error': 'Pet profile not found'}), 404
+    if any(current_user.id == pet_profile.user_id for profile in pet_profiles):
+        new_calorie_goal=Calorie_Goal(
+            profile_id=pet_profile.id,
+            calorie_goal=form.calorie_goal.data,
+            date=form.date.data
+        )
+        db.session.add(new_calorie_goal)
+        db.session.commit()
+        return new_calorie_goal.to_dict()
+    return jsonify({'error': 'Pet you are trying to make a caloric goal for does not belong to you.'}), 401
 
 
 # WEIGHT GOAL ROUTES IF I WANT TO CONNECT WEIGHT INFORMATION TO PET PROFILE
@@ -172,13 +183,20 @@ def post_one_weight_goal(id):
     form=WeightForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
-    profile_id=Profile.query.get(id)
-    new_weight_goal=Weight_Goal(
-        profile_id=profile_id.id,
-        weight_goal=form.weight_goal.data,
-        current_weight=form.current_weight.data,
-        date=form.date.data
-    )
-    db.session.add(new_weight_goal)
-    db.session.commit()
-    return new_weight_goal.to_dict()
+    pet_profile=Profile.query.get(id)
+    pet_profiles=Profile.query.filter(Profile.user_id==current_user.id).all()
+
+    if not pet_profile:
+        return jsonify({'error': 'Pet profile not found'}), 404
+    if any(current_user.id == pet_profile.user_id for profile in pet_profiles):
+
+        new_weight_goal=Weight_Goal(
+            profile_id=pet_profile.id,
+            weight_goal=form.weight_goal.data,
+            current_weight=form.current_weight.data,
+            date=form.date.data
+        )
+        db.session.add(new_weight_goal)
+        db.session.commit()
+        return new_weight_goal.to_dict()
+    return jsonify({'error': 'Pet you are trying to make a weight goal for does not belong to you.'}), 401
